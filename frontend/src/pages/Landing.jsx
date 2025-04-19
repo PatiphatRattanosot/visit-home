299a93631849daa9dd4aea0e5766f26de0f7becf
TanKittipat <kittipat12tan@gmail.com>
2025-04-19 19:39:34 +0700

    Update hero

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,18 +1,17 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-
 const Landing = () => {
   return (
     <>
-      <Navbar />
-      <div className="h-[68.5vh]">
-        <img
-          src="hero.webp"
-          alt="hero"
-          className="h-full w-full object-cover"
-        />
+      <div
+        className="relative h-[68.5vh] bg-cover bg-center"
+        style={{ backgroundImage: "url('hero.webp')" }} // Corrected inline style syntax
+      >
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="relative flex items-center justify-center h-full">
+          <h1 className="text-white text-4xl md:text-6xl font-bold text-center">
+            ระบบบันทึกการเยี่ยมบ้านโรงเรียนบางแพปฐมพิทยา
+          </h1>
+        </div>
       </div>
-      <Footer />
     </>
   );
 };
