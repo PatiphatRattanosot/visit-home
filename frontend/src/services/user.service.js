d5bd89dcef5ac673cefc0d46f2336dec82db22b7
Punsano26 <xlichaphlreuxn@gmail.com>
2025-04-30 19:22:26 +0700

    instant mockup api

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,21 +1,7 @@
-// api/userApi.js
-import axios from "axios";
-
-export const fetchUsers = async () => {
-  try {
-    const res = await axios.get("http://localhost:3000/user");
-
-    return res.data; // ส่งข้อมูลกลับไป
-  } catch (err) {
-    console.error("ไอสัส", err, "เชี่ยแล้วนะจ๊ะ");
-  }
-};
+import api from "./api"; // หรือ axios instance ที่คุณตั้งไว้
 
 export const deleteUser = async (id) => {
-  try {
-    const res = await axios.delete(`http://localhost:3000/user/${id}`);
-    return res.data; // ส่งข้อมูลกลับไป
-  } catch (err) {
-    console.error("ไอสัส", err, "เชี่ยแล้วนะจ๊ะ");
-  }
-}
+  const res = await api.delete(`http://localhost:3000/user/${id}`);
+  return res.data;
+};
+
