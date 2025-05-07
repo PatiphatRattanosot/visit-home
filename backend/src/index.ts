642df88546ebf5d1218062ac9aa879823adb4a17
patiphatrattanosot <654259017@webmail.npru.ac.th>
2025-05-07 13:54:49 +0700

    feat: add CRUD operations for year management in YearController

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -87,7 +87,13 @@ const app = new Elysia()
       // Student
       // Year
       .group("/year", (app) =>
-        app.use(YearController.create_year).use(YearController.auto_create_year)
+        app
+          .use(YearController.create_year)
+          .use(YearController.auto_create_year)
+          .use(YearController.get_years)
+          .use(YearController.get_year_by_id)
+          .use(YearController.update_year)
+          .use(YearController.delete_year)
       )
   )
   // Home Page
