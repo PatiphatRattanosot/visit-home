fb2221ef0b48be02e3f43624ca606a404ba28873
TanKittipat <kittipat12tan@gmail.com>
2025-05-13 19:25:14 +0700

    Make update page

--- frontend/src/schemas/relation.js
+++ frontend/src/schemas/relation.js
@@ -6,63 +6,31 @@ export const RelationSchema = yup.object().shape({
   family_member: yup
     .number()
     .min(1, "กรุณากรอกจำนวนให้ถูกต้อง")
-    .integer("จำนวนสมาชิกไม่ควรเป็นทศนิยม")
-    .required("กรุณากรอกจำนวนสมาชิกในครอบครัว"),
-  family_time: yup
-    .number()
-    .min(0, "กรุณากรอกจำนวนชั่วโมงให้ถูกต้อง")
-    .required("กรุณากรอกจำนวนชั่วโมงที่อยู่ร่วมกัน"),
-  father_relation: yup
-    .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
-  mother_relation: yup
-    .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
-  brother_relation: yup
-    .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
-  sister_relation: yup
-    .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
+    .integer("จำนวนสมาชิกไม่ควรเป็นทศนิยม"),
+  family_time: yup.number().min(0, "กรุณากรอกจำนวนชั่วโมงให้ถูกต้อง"),
+  father_relation: yup.string().matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
+  mother_relation: yup.string().matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
+  brother_relation: yup.string().matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
+  sister_relation: yup.string().matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
   grand_parent_relation: yup
     .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
-  relatives_relation: yup
-    .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
+    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
+  relatives_relation: yup.string().matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
   other_relative: yup.string().matches(onlyThaiLang, "กรุณากรอกเป็นภาษาไทย"),
   other_relation: yup.string().matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
-  when_student_alone: yup
-    .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
-  total_household_income: yup
-    .number()
-    .min(0, "กรุณากรอกจำนวนเงินให้ถูกต้อง")
-    .required("กรุณากรอกรายได้ของครัวเรือน"),
-  daily_total_to_school: yup
-    .number()
-    .min(0, "กรุณากรอกจำนวนเงินให้ถูกต้อง")
-    .required("กรุณากรอกเงินค่าขนมที่ได้รับ"),
+  when_student_alone: yup.string().matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
+  total_household_income: yup.number().min(0, "กรุณากรอกจำนวนเงินให้ถูกต้อง"),
+  daily_total_to_school: yup.number().min(0, "กรุณากรอกจำนวนเงินให้ถูกต้อง"),
   received_daily_from: yup
     .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
+    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
   student_part_time: yup.string().matches(onlyThaiLang, "กรุณากรอกเป็นภาษาไทย"),
   student_income: yup.number().min(0, "กรุณากรอกจำนวนเงินให้ถูกต้อง"),
   support_from_school: yup
     .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
+    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
   support_from_organize: yup
     .string()
-    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย")
-    .required("กรุณาเลือกคำตอบ"),
+    .matches(onlyThaiLang, "กรุณาตอบเป็นภาษาไทย"),
   parent_concern: yup.string().matches(onlyThaiLang, "กรุณากรอกเป็นภาษาไทย"),
 });
