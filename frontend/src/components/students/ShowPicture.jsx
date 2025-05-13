3d96e804c6407f2f706882a39a8bd29359186590
TanKittipat <kittipat12tan@gmail.com>
2025-05-13 19:30:00 +0700

    Edit style in show data page

--- frontend/src/components/students/ShowPicture.jsx
+++ frontend/src/components/students/ShowPicture.jsx
@@ -1,6 +1,6 @@
 const ShowPicture = ({ studentPic }) => {
   return (
-    <div className="border-1 border-gray-200 bg-white w-[13.875rem] h-[18.5rem] rounded-md flex justify-center items-center">
+    <div className="border-1 border-gray-100 bg-white w-[13.875rem] h-[18.5rem] rounded-md flex justify-center items-center">
       {studentPic ? (
         <img
           src={studentPic}
@@ -8,7 +8,9 @@ const ShowPicture = ({ studentPic }) => {
           className="h-full w-full bg-cover object-cover rounded-md p-1"
         />
       ) : (
-        <div className="bg-gray-100 w-full h-full object-cover rounded-md p-1"></div>
+        <div className="bg-gray-50 w-full h-full object-cover rounded-md p-1 text-gray-500 flex justify-center items-center">
+          ยังไม่มีรูปนักเรียน
+        </div>
       )}
     </div>
   );
