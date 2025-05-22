7020e113086658edba237b913767cbd38d2403b8
TanKittipat <kittipat12tan@gmail.com>
2025-05-22 22:13:34 +0700

    Make add data and update data page and make validation schema

--- frontend/src/pages/students/behavior/AddBehaviorForm.jsx
+++ frontend/src/pages/students/behavior/AddBehaviorForm.jsx
@@ -3,6 +3,10 @@ import { useParams, useNavigate } from "react-router";
 import { useAuthStore } from "../../../stores/auth.store";
 import Stepper from "../../../components/Stepper";
 import CheckboxInput from "../../../components/CheckboxInput";
+import RadioInput from "../../../components/RadioInput";
+import TextInput from "../../../components/TextInput";
+import SelectInput from "../../../components/SelectInput";
+import { BehaviorSchema } from "../../../schemas/behavior";
 
 const AddBehaviorForm = () => {
   const { userInfo } = useAuthStore();
@@ -11,6 +15,7 @@ const AddBehaviorForm = () => {
     initialValues,
     values,
     setValues,
+    setFieldValue,
     errors,
     touched,
     handleBlur,
@@ -20,12 +25,26 @@ const AddBehaviorForm = () => {
     initialValues: {
       health_risk: [],
       welfare_and_safety: [],
+      distance_to_school: 0,
+      time_used: 0,
+      school_transport: "",
+      student_responsibilities: [],
+      hobbies: [],
+      drugs_behavior: [],
+      violent_behavior: [],
+      sexual_behavior: [],
+      gaming_behavior: [],
+      computer_internet_access: "",
+      tech_use_behavior: "",
+      information_giver: "",
     },
+    validationSchema: BehaviorSchema,
     onSubmit: async (values, actions) => {
       console.log(values);
       actions.resetForm();
     },
   });
+  console.log(values);
 
   const { year } = useParams();
   const navigate = useNavigate();
@@ -36,6 +55,24 @@ const AddBehaviorForm = () => {
     stepThree: `/student/visit-info/${year}/family-status/add`,
     stepFour: `/student/visit-info/${year}/behavior/add`,
   };
+
+  const familyMember = [
+    "บิดา",
+    "มารดา",
+    "พี่ชาย",
+    "พี่สาว",
+    "ลุง",
+    "ป้า",
+    "น้า",
+    "อา",
+    "ปู่",
+    "ย่า",
+    "ตา",
+    "ยาย",
+    "ทวด",
+    "พ่อเลี้ยง",
+    "แม่เลี้ยง",
+  ];
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 py-9">
       <div className="w-full max-w-5xl p-6 bg-white rounded-lg shadow-md">
@@ -97,6 +134,223 @@ const AddBehaviorForm = () => {
               touched={touched.welfare_and_safety}
               grid="grid-cols-2"
             />
+            {/* group การเดินทาง */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {/* ระยะทางไปโรงเรียน */}
+              <TextInput
+                type="number"
+                label={"ระยะทางจากบ้านไปโรงเรียนประมาณ (กิโลเมตร)"}
+                name={"distance_to_school"}
+                value={values.distance_to_school}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.distance_to_school}
+                touched={touched.distance_to_school}
+                placeholder={"กรอกระยะทางไปโรงเรียน"}
+              />
+              {/* เวลาเดินทาง */}
+              <TextInput
+                type="number"
+                label={"เวลาที่ใช้เดินทางโดยประมาณ (ชั่วโมง)"}
+                name={"time_used"}
+                value={values.time_used}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.time_used}
+                touched={touched.time_used}
+                placeholder={"กรอกเวลาที่ใช้ในการเดินทาง"}
+              />
+            </div>
+            {/* วิธีการเดินทาง */}
+            <RadioInput
+              label={"วิธีการเดินทางของนักเรียนไปโรงเรียน"}
+              name={"school_transport"}
+              value={values.school_transport}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.school_transport}
+              touched={touched.school_transport}
+              options={[
+                "ผู้ปกครองมาส่ง",
+                "รถโดยสารประจำทาง",
+                "รถจักรยานยนต์",
+                "รถโรงเรียน",
+                "รถยนต์",
+                "รถจักรยาน",
+                "เดิน",
+              ]}
+              extraOpt
+              grid="grid-cols-2"
+            />
+            {/* ภาระงานความรับผิดชอบของนักเรียนที่มีต่อครอบครัว */}
+            <CheckboxInput
+              label={
+                "ภาระงานความรับผิดชอบของนักเรียนที่มีต่อครอบครัว (ตอบได้มากกว่า 1 ข้อ)"
+              }
+              name={"student_responsibilities"}
+              value={values.student_responsibilities}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.student_responsibilities}
+              touched={touched.student_responsibilities}
+              options={[
+                "ช่วยงานบ้าน",
+                "ช่วยคนดูแลคนเจ็บป่วย/พิการ",
+                "ช่วยค้าขายเล็กๆน้อยๆ",
+                "ทำงานพิเศษแถวบ้าน",
+                "ช่วยงานในนาไร่",
+              ]}
+              extraOpt
+              grid="grid-cols-2"
+              setFieldValue={setFieldValue}
+            />
+            {/* กิจกรรมยามว่างหรืองานอดิเรก */}
+            <CheckboxInput
+              label={"กิจกรรมยามว่างหรืองานอดิเรก (ตอบได้มากกว่า 1 ข้อ)"}
+              name={"hobbies"}
+              value={values.hobbies}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.hobbies}
+              touched={touched.hobbies}
+              options={[
+                "ดูทีวี/ ฟังเพลง",
+                "ไปเที่ยวห้าง/ ดูหนัง",
+                "อ่านหนังสือ",
+                "ไปหาเพื่อน/ เพื่อน",
+                "แว้น/ สก๊อย",
+                "เล่นเกม คอม / มือถือ",
+                "ไปสวนสาธารณะ",
+                "เล่นดนตรี",
+              ]}
+              extraOpt
+              grid="grid-cols-2"
+              setFieldValue={setFieldValue}
+            />
+            {/* พฤติกรรมการใช้สารเสพติด */}
+            <CheckboxInput
+              label={"พฤติกรรมการใช้สารเสพติด (ตอบได้มากกว่า 1 ข้อ)"}
+              name={"drugs_behavior"}
+              value={values.drugs_behavior}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.drugs_behavior}
+              touched={touched.drugs_behavior}
+              options={[
+                "คบเพื่อนในกลุ่มที่ใช้สารเสพติด",
+                "สมาชิกในครอบครัวข้องเกี่ยวกับยาเสพติด",
+                "อยู่ในสภาพแวดล้อมที่ใช้สารเสพติด",
+                "ปัจจุบันเกี่ยวข้องกับสารเสพติด",
+                "เป็นผู้ติดบุหรี่ สุรา หรือการใช้สารเสพติดอื่นๆ",
+              ]}
+              grid="grid-cols-2"
+            />
+            {/* พฤติกรรมการใช้ความรุนแรง */}
+            <CheckboxInput
+              label={"พฤติกรรมการใช้ความรุนแรง (ตอบได้มากกว่า 1 ข้อ)"}
+              name={"violent_behavior"}
+              value={values.violent_behavior}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.violent_behavior}
+              touched={touched.violent_behavior}
+              options={[
+                "มีการทะเลาะวิวาท",
+                "ก้าวร้าว เกเร",
+                "ทะเลาะวิวาทเป็นประจำ",
+                "ทำร้ายร่างกายผู้อื่น",
+                "ทำร้ายร่างกายตนเอง",
+              ]}
+              extraOpt
+              grid="grid-cols-2"
+              setFieldValue={setFieldValue}
+            />
+            {/* พฤติกรรมทางเพศ */}
+            <CheckboxInput
+              label={"พฤติกรรมทางเพศ (ตอบได้มากกว่า 1 ข้อ)"}
+              name={"sexual_behavior"}
+              value={values.sexual_behavior}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.sexual_behavior}
+              touched={touched.sexual_behavior}
+              options={[
+                "อยู่ในกลุ่มขายบริการ",
+                "ใช้เครื่องมือสื่อสารที่เกี่ยวข้องกับด้านเพศเป็นเวลานานและบ่อยครั้ง",
+                "ตั้งครรภ์",
+                "ขายบริการทางเพศ",
+                "หมกมุ่นในการใช้เครื่องมือสื่อสารที่เกี่ยวข้องทางเพศ",
+                "มีการมั่วสุมทางเพศ",
+              ]}
+              grid="grid-cols-2"
+            />
+            {/* การติดเกม */}
+            <CheckboxInput
+              label={"การติดเกม (ตอบได้มากกว่า 1 ข้อ)"}
+              name={"gaming_behavior"}
+              value={values.gaming_behavior}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.gaming_behavior}
+              touched={touched.gaming_behavior}
+              options={[
+                "เล่นเกมเกินวันละ 1 ชั่วโมง",
+                "ขาดจินตนาการและความคิดสร้างสรรค์",
+                "เก็บตัว แยกตัวจากกลุ่มเพื่อน",
+                "ใช้จ่ายเงินผิดปกติ",
+                "อยู่ในกลุ่มเพื่อนเล่นเกม",
+                "ร้านเกมอยู่ใกล้บ้านหรือโรงเรียน",
+                "ใช้เวลาเล่นเกมเกิน 2 ชั่วโมง",
+                "หมกมุ่น จริงจังในการเล่นเกม",
+                "ใช้เงินสิ้นเปลือง โกหก ลักขโมยเงินเพื่อเล่นเกม",
+              ]}
+              extraOpt
+              grid="grid-cols-2"
+              setFieldValue={setFieldValue}
+            />
+            {/* การเข้าถึงสื่อคอมพิวเตอร์และอินเตอร์เน็ตที่บ้าน */}
+            <RadioInput
+              label={"การเข้าถึงสื่อคอมพิวเตอร์และอินเตอร์เน็ตที่บ้าน"}
+              name={"computer_internet_access"}
+              value={values.computer_internet_access}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.computer_internet_access}
+              touched={touched.computer_internet_access}
+              options={[
+                "สามารถเข้าถึง Internet ได้จากที่บ้าน",
+                "ไม่สามารถเข้าถึง Internet ได้จากที่บ้าน",
+              ]}
+              grid="grid-cols-2"
+            />
+            {/* การใช้เครื่องมือสื่อสารอิเล็กทรอนิกส์ */}
+            <RadioInput
+              label={"การใช้เครื่องมือสื่อสารอิเล็กทรอนิกส์"}
+              name={"tech_use_behavior"}
+              value={values.tech_use_behavior}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.tech_use_behavior}
+              touched={touched.tech_use_behavior}
+              options={[
+                "ใช้ Social media/game (ไม่เกินวันละ 3 ชั่วโมง)",
+                "ใช้ Social media/game (วันละ 3 ชั่วโมงขึ้นไป)",
+              ]}
+              grid="grid-cols-2"
+            />
+            {/* ผู้ให้ข้อมูล */}
+            <SelectInput
+              label={"ผู้ให้ข้อมูลนักเรียน"}
+              name={"information_giver"}
+              value={values.information_giver}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={errors.information_giver}
+              touched={touched.information_giver}
+              options={familyMember}
+              defaultOpt={"ผู้ให้ข้อมูล"}
+              className="w-1/2 pr-3"
+            />
           </div>
           <div className="flex justify-between mt-10 space-x-2">
             <button
@@ -108,8 +362,8 @@ const AddBehaviorForm = () => {
             >
               ก่อนหน้า
             </button>
-            <button type="submit" className="btn-gray w-1/2">
-              ถัดไป
+            <button type="submit" className="btn-green w-1/2">
+              บันทึก
             </button>
           </div>
         </form>
--- frontend/src/pages/students/behavior/Behavior.jsx
+++ frontend/src/pages/students/behavior/Behavior.jsx
@@ -16,7 +16,7 @@ const Behavior = () => {
       try {
         const res = await axios.get("http://localhost:5000/studentInfo/1");
         if (res.status === 200) {
-          setBehaviorInfo(null);
+          setBehaviorInfo(res?.data?.behavior_and_risk[0]);
         }
       } catch (error) {
         console.log("บัคดึงข้อมูล", error);
@@ -74,8 +74,141 @@ const Behavior = () => {
               </h3>
               {behaviorInfo !== null ? (
                 <div className="text-left flex flex-col gap-2.5">
-                  <div className="grid grid-cols-1 gap-x-6 gap-y-4 text-gray-600">
-                    {/* Data here */}
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4 text-gray-600">
+                    {/* สุขภาพ */}
+                    <div>
+                      ด้านสุขภาพ:{" "}
+                      <span className="text-black">
+                        {behaviorInfo?.health_risk.length === 0
+                          ? "ไม่มีความเสี่ยง"
+                          : behaviorInfo.health_risk.join(", ")}
+                      </span>
+                    </div>
+                    {/* สวัสดิการหรือความปลอดภัย */}
+                    <div>
+                      สวัสดิการหรือความปลอดภัย:{" "}
+                      <span className="text-black">
+                        {behaviorInfo?.welfare_and_safety.length === 0
+                          ? "ไม่มีความเสี่ยง"
+                          : behaviorInfo.welfare_and_safety.join(", ")}
+                      </span>
+                    </div>
+                    {/* ระยะทางระหว่างบ้านไปโรงเรียน */}
+                    <div>
+                      ระยะทางระหว่างบ้านไปโรงเรียน:{" "}
+                      <span className="text-black">
+                        {behaviorInfo?.distance_to_school}
+                      </span>{" "}
+                      กิโลเมตร
+                    </div>
+                    {/* ใช้เวลาเดินทางประมาณ */}
+                    <div>
+                      ใช้เวลาเดินทางประมาณ:{" "}
+                      <span className="text-black">
+                        {behaviorInfo?.time_used}
+                      </span>{" "}
+                      ชั่วโมง
+                    </div>
+                    {/* การเดินทางของนักเรียนไปโรงเรียน */}
+                    <div>
+                      การเดินทางของนักเรียนไปโรงเรียน:{" "}
+                      <span className="text-black">
+                        {behaviorInfo?.school_transport}
+                      </span>
+                    </div>
+                    <div className="md:col-span-2">
+                      {/* ภาระงานความรับผิดชอบของนักเรียนที่มีต่อครอบครัว */}
+                      <div>
+                        ภาระงานความรับผิดชอบของนักเรียนที่มีต่อครอบครัว:{" "}
+                        <span className="text-black">
+                          {behaviorInfo?.student_responsibilities.length === 0
+                            ? "ไม่มีความเสี่ยง"
+                            : behaviorInfo.student_responsibilities.join(", ")}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="md:col-span-2">
+                      {/* กิจกรรมยามว่างหรืองานอดิเรก */}
+                      <div>
+                        กิจกรรมยามว่างหรืองานอดิเรก:{" "}
+                        <span className="text-black">
+                          {behaviorInfo?.hobbies.length === 0
+                            ? "ไม่มีความเสี่ยง"
+                            : behaviorInfo.hobbies.join(", ")}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="md:col-span-2">
+                      {/* พฤติกรรมการใช้สารเสพติด */}
+                      <div>
+                        พฤติกรรมการใช้สารเสพติด:{" "}
+                        <span className="text-black">
+                          {behaviorInfo?.drugs_behavior.length === 0
+                            ? "ไม่มีความเสี่ยง"
+                            : behaviorInfo.drugs_behavior.join(", ")}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="md:col-span-2">
+                      {/* พฤติกรรมการใช้ความรุนแรง */}
+                      <div>
+                        พฤติกรรมการใช้ความรุนแรง:{" "}
+                        <span className="text-black">
+                          {behaviorInfo?.violent_behavior.length === 0
+                            ? "ไม่มีความเสี่ยง"
+                            : behaviorInfo.violent_behavior.join(", ")}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="md:col-span-2">
+                      {/* พฤติกรรมทางเพศ */}
+                      <div>
+                        พฤติกรรมทางเพศ:{" "}
+                        <span className="text-black">
+                          {behaviorInfo?.sexual_behavior.length === 0
+                            ? "ไม่มีความเสี่ยง"
+                            : behaviorInfo.sexual_behavior.join(", ")}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="md:col-span-2">
+                      {/* การติดเกม */}
+                      <div>
+                        การติดเกม:{" "}
+                        <span className="text-black">
+                          {behaviorInfo?.gaming_behavior.length === 0
+                            ? "ไม่มีความเสี่ยง"
+                            : behaviorInfo.gaming_behavior.join(", ")}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="md:col-span-2">
+                      {/* การเข้าถึงสื่อคอมพิวเตอร์และอินเตอร์เน็ตที่บ้าน */}
+                      <div>
+                        การเข้าถึงสื่อคอมพิวเตอร์และอินเตอร์เน็ตที่บ้าน:{" "}
+                        <span className="text-black">
+                          {behaviorInfo?.computer_internet_access}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="md:col-span-2">
+                      {/* การใช้เครื่องมือสื่อสารอิเล็กทรอนิกส์ */}
+                      <div>
+                        การใช้เครื่องมือสื่อสารอิเล็กทรอนิกส์:{" "}
+                        <span className="text-black">
+                          {behaviorInfo?.tech_use_behavior}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="md:col-span-2">
+                      {/* ผู้ให้ข้อมูลนักเรียน */}
+                      <div className="font-bold">
+                        ผู้ให้ข้อมูลนักเรียน:{" "}
+                        <span className="text-black">
+                          {behaviorInfo?.information_giver}
+                        </span>
+                      </div>
+                    </div>
                   </div>
                 </div>
               ) : (
